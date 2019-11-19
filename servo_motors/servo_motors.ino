70e810b6790925e89fbf035ad172d52c10660c27
Christopher Ho <christopherho@v1040-wn-rt-b-87-195.campus-dynamic.uwaterloo.ca>
2019-11-19 15:02:29 -0500

    Added template code and changed instructions

--- servo_motors/servo_motors.ino
+++ servo_motors/servo_motors.ino
@@ -25,11 +25,10 @@ const int SERVOCHG = 20; // 'change' pulse length count
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 String valInput; // Serial input var.
-int i=0; // loop index var.
 int val[6] = {309, 325, 330, 287, 310, 332}; // PWM var
-int mins[6] = {140, 132, 142, 113, 136, 138}; // lower bounds for each motor
-int mids[6] = {309, 325, 330, 287, 310, 332}; // reset points
-int maxs[6] = {478, 518, 518, 462, 485, 527}; // upper bounds for each motor
+int mins[6] = {140, 132, 142, 90, 90, 138}; // lower bounds for each motor
+int mids[6] = {309, 325, 330, 295, 295, 332}; // reset points
+int maxs[6] = {478, 518, 518, 500, 500, 527}; // upper bounds for each motor
 
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
@@ -40,21 +39,26 @@ void setup() {
 }
 
 void balance(){
-   for (i=0; i<6; i++) {
+   for (int i=0; i<6; i++) {
        val[i] = mids[i];
    }
    update();
 }
 
 void update(){
-  for (i=0; i<6; i++) {
+  for (int i=0; i<6; i++) {
      pwm.setPWM(i+1, 0, val[i]); // added +1 to match PWM port numbering (pins 1..6 used)
-  }
+  };
+  Serial.print(" Servo values = [");
+    for (int i=0; i<6; i++) {
+      Serial.print(val[i]);
+      Serial.print(" ");
+    }
+    Serial.println("]");
 }
 
 void loop() {
   if (Serial.available() > 0) {
-
     valInput = Serial.readString();
     Serial.print("I received: ");
     Serial.print(valInput);
@@ -72,140 +76,86 @@ void loop() {
       case '@':
         balance();
         delay(3000);
-        val[0]= min( val[0]+100, maxs[0] );
-        val[4]= max( val[4]-100, mins[4] );
+
+        // move ball to corner
+        val[5]= min( val[5]+100, maxs[5] );
+        val[3]= max( val[3]-100, mins[3] );
         update();
         delay(3000);
-        val[3]= max( val[3]-SERVOCHG, mins[3] );
+
         val[5]= min( val[5]+SERVOCHG, maxs[5] );
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
-        update();
-        delay(2000);
         val[3]= max( val[3]-SERVOCHG, mins[3] );
-        val[5]= min( val[5]+SERVOCHG, maxs[5] );
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
         update();
         delay(2000);
-        val[3]= max( val[3]-SERVOCHG, mins[3] );
-        val[5]= min( val[5]+SERVOCHG, maxs[5] );
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
+
+        //begin slight tilts, repeat
+        val[0]= max( val[0]-SERVOCHG, mins[0]);
+        val[4]= max( val[4]-SERVOCHG, mins[4]);
         update();
         delay(2000);
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
-        update();
-        delay(2000);
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
-        update();
-        delay(2000);
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
-        update();
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
-        update();
-        delay(700);
-        val[3]= max( val[3]-100, mins[3] );
-        val[5]= min( val[5]+100, maxs[5] );
-        update();
-        //up to here, the ball should have made the sharp turn
         
-        delay(1100);
-        val[3]= max( val[3]-100, mins[3] );
-        val[5]= min( val[5]+100, maxs[5] );
+        val[0]= max( val[0]-SERVOCHG, mins[0]);
+        val[4]= max( val[4]-SERVOCHG, mins[4]);
         update();
+        delay(2000);
         
+        val[0]= max( val[0]-SERVOCHG, mins[0]);
+        val[4]= max( val[4]-SERVOCHG, mins[4]);
+        update();
+        delay(2000);
+
+        val[0]= max( val[0]-SERVOCHG, mins[0]);
+        val[4]= max( val[4]-SERVOCHG, mins[4]);
+        update();
+        delay(2000);
+
+        val[0]= max( val[0]-SERVOCHG, mins[0]);
+        val[4]= max( val[4]-SERVOCHG, mins[4]);
+        update();
+        delay(750);
+
+        // hit the sharp turn right here
+        val[1]= max( val[1]-50, mins[1] );
+        val[2]= min( val[2]+50, maxs[2] );
+        val[0] = mids[0];
+        update();
+        delay(3000);
+
+        // straighten up
+        val[5] = mids[5];
+        val[0] = max( val[0]-SERVOCHG, mins[0]);
+        update();
+        delay(3000);
+
+        // tilt ball into finish position
+        val[0]= max( val[0]-SERVOCHG, mins[0]);
+        val[4]= max( val[4]-SERVOCHG, mins[4]);
+        update();
+        delay(2000);
+
+        val[0]= max( val[0]-SERVOCHG, mins[0]);
+        val[4]= max( val[4]-SERVOCHG, mins[4]);
+        update();
+        delay(2000);
+   
         Serial.println("Finished Solving");
-        break;
-        
-      case 'i': 
-        val[0]= min( val[0]+SERVOCHG, maxs[0] );
-        val[4]= max( val[4]-SERVOCHG, mins[4] );
-        break;
+        break;   
 
-      case 'o': 
-        val[0]= max( val[0]-SERVOCHG, mins[0] );
-        val[4]= min( val[4]+SERVOCHG, maxs[4] );
-        break;
-
-      // motor 4,6 paired
-      case 'k': 
-        val[3]= max( val[3]-SERVOCHG, mins[3] );
-        val[5]= min( val[5]+SERVOCHG, maxs[5] );
-        break;
-        
-      case 'l': 
-        val[3]= min( val[3]+SERVOCHG, maxs[3] );
-        val[5]= max( val[5]-SERVOCHG, mins[5] );
-        break;
-
-      // motor 2, 3
-      case 'b': 
-        val[1]= min( val[1]+SERVOCHG, maxs[1] );
-        val[2]= max( val[2]-SERVOCHG, mins[2] );
-        break;
-        
-      case 'n': 
-        val[1]= max( val[1]-SERVOCHG, mins[1] );
-        val[2]= min( val[2]+SERVOCHG, maxs[2] );
-        break;
-
-      case '9':
-        val[2]= max( val[2]-SERVOCHG, mins[2] );
-        val[3]= max( val[3]-SERVOCHG, mins[3] );
-        break;
-
-      case '0':
-        val[3]= max( val[3]-150, mins[3] );
-        val[5]= max( val[5]-SERVOCHG, mins[5] );
-        delay(1000);
-        break;
-        
-
-//      case 'l': 
-//        val[1]= min( val[1]+SERVOCHG, maxs[1] );
-//        val[4]= min( val[4]-SERVOCHG, maxs[4] );
-//        break;
-//      case 'j': 
-//        val[1]= max( val[1]-SERVOCHG, mins[1] );
-//        val[4]= max( val[4]+SERVOCHG, mins[4] );
-//        break;
-//
-//       
-//      case 'u': 
-//        val[2]= min( val[2]+SERVOCHG, maxs[2] );
-//        val[5]= min( val[5]-SERVOCHG, maxs[5] );
-//        break;
-//      case 'o': 
-//        val[2]= max( val[2]-SERVOCHG, mins[2] );
-//        val[5]= max( val[5]+SERVOCHG, mins[5] );
-//        break;
-
-      // reset case for middle positions of all motors
+       // reset case for middle positions of all motors
       case 'm':
-        for (i=0; i<6; i++) {
-          val[i] = mids[i];
-        }
+        balance();
+        break;
+
+      case '1': 
+        val[0]= min( val[0]+SERVOCHG, SERVOMAX );
+        update();
+        break;
+      case 'q': 
+        val[0]= max( val[0]-SERVOCHG, SERVOMIN );
         update();
         break;
         
       default: Serial.print(" No action taken");
     } // end switch statement
-    
-    Serial.print(" Servo values = [");
-    for (i=0; i<6; i++) {
-      Serial.print(val[i]);
-      Serial.print(" ");
-    }
-    Serial.println("]");
-
-    // Update servo commands:
-//    for (i=0; i<6; i++) {
-//      pwm.setPWM(i+1, 0, val[i]); // added +1 to match PWM port numbering (pins 1..6 used)
-//    }
   }
 }
